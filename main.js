14c65de7e789758611b328c3a8c47bd59854aceb
agnesvarga1 <agikolyok@gmail.com>
2023-12-22 15:33:17 +0100

    getting kms and age from user

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert("Si parte");
+let ticketkms = document.querySelector("#trip-km");
+let age = document.querySelector("#age-input");
+const buyTicketBtn = document.querySelector("#buyTicketBtn");
+const errMsg = document.querySelector(".err");
+console.log(errMsg);
+buyTicketBtn.addEventListener("click", function () {
+  console.log(ticketkms.value, age.value);
+  if (ticketkms.value == 0 || !ticketkms) {
+    errMsg.innerHTML = "Must fill km field and can not be 0";
+  }
+});
